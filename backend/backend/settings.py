501f24379c920722ca1617b362ed64569234c994
Vishnu Kumar <mjvishnukumar2003@gmail.com>
2024-08-23 22:21:49 +0530

    New database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -95,13 +95,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD':os.getenv("DB_PWD"),
-        'PORT':os.getenv("DB_PORT"),
-        'HOST':os.getenv("DB_HOST"),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PWD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
